ebcb9a9f56709b28915c69f70c00747bb1043941
robocopkaka <robocopkaka@gmail.com>
2016-10-20 16:46:41 +0100

    Fixed issues for getting pay for managers and executive

--- salary.js
+++ salary.js
@@ -4,45 +4,55 @@ function salary(level, yearsOfExperience){
 
     var manager = 500000
     var executive = 1000000
+    
 
-    this.calcManager = function(){
+    
+
+    if(this.level === "manager"){
     	if(this.yearsOfExperience === 1){
-    		return this.salary = ((manager * 0.1) + manager)
+    		this.salary = ((manager * 0.1) + manager);
+    		return this.salary;
     	}
     	else if(yearsOfExperience === 2){
-    		return this.salary = ((manager * 0.2) + manager)
+    		this.salary = ((manager * 0.2) + manager);
+    		return this.salary;
     	}
     	else if(yearsOfExperience === 3){
-    		return this.salary = ((manager * 0.3) + manager)
+    		this.salary = ((manager * 0.3) + manager);
+    		return this.salary;
     	}
 
     	else if(yearsOfExperience === 4){
-    		return this.salary = ((manager * 0.4) + manager)
+    		this.salary = ((manager * 0.4) + manager)
+    		return this.salary;
     	}
     	else if(yearsOfExperience >= 5){
-    		return this.salary = ((manager * 0.5) + manager)
+    		this.salary = ((manager * 0.5) + manager)
+    		return this.salary;
     	}
     }
 
-    this.calcExecutive = function(){
+    if(this.level === "executive"){
     	if(this.yearsOfExperience === 1){
-    		return this.salary = ((executive * 0.1) + executive)
+    		this.salary = ((executive * 0.1) + executive)
+    		return this.salary;
     	}
     	else if(yearsOfExperience === 2){
-    		return this.salary = ((executive * 0.2) + executive)
+    		this.salary = ((executive * 0.2) + executive)
+    		return this.salary;
     	}
     	else if(yearsOfExperience === 3){
-    		return this.salary = ((executive * 0.3) + executive)
+    		this.salary = ((executive * 0.3) + executive)
+    		return this.salary;
     	}
 
     	else if(yearsOfExperience === 4){
-    		return this.salary = ((executive * 0.4) + executive)
+    		this.salary = ((executive * 0.4) + executive)
+    		return this.salary;
     	}
     	else if(yearsOfExperience >= 5){
-    		return this.salary = ((executive * 0.5) + executive)
+    		this.salary = ((executive * 0.5) + executive)
+    		return this.salary;
     	}
     }
-
-    this
-
-};
+}
